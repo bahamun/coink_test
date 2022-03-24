5b7379e07988eeea566e454962aa7f266aa3d1af
unknown <bahamun318@gmail.com>
2022-03-24 08:56:58 -0500

    coink all

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,8 @@ export const environment = {
   production: true,
   host: {
     coinApi: 'https://api.bancoink.biz/qa/',
+    // eslint-disable-next-line max-len
+    key: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJBcGlLZXkiOiIwMzAxMDYiLCJWZXJzaW9uIjoiMS4wLjAifQ.OJ7pEEf3b0tPHwdWIn7-v18tYnMeYhTU9UT8zDSEtrg',
+    apiKey: '030106',
   },
 };
